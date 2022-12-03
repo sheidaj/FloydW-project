b62baad8a431e91c03af69c14478501133d203a6
#Sheida <41925038+sheidaj@users.noreply.github.com>
2022-12-03 21:49:09 +0000

    Update Samples.py

--- Test/Samples.py
+++ Test/Samples.py
@@ -1,44 +1,42 @@
 # Test samples for Floyd Warshall Function
 
-""" 
-This file contains four different samples for 
-the Floyd Warshall algorithm function.
+""" This file contains four different samples for the Floyd Warshall
+algorithm function.
 """
 
 # Use value for infinity to denote edges that do not exist
-INF = 999999
+import itertools
+import sys
+#Npath is no direct path
+NPATH = sys.maxsize
 
-# Test A: Control Graph
-""" Title: Floyd Warshall Algorithm | DP-16
-Author: GeeksforGeeks
-Availability: https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
-"""
-sample_a = [[0, 5, INF, 10],
-            [INF, 0, 3, INF],
-            [INF, INF, 0, 1],
-            [INF, INF, INF, 0]]
 
-output_a = [[0, 5, 8, 9],
-            [INF, 0, 3, 4],
-            [INF, INF, 0, 1],
-            [INF, INF, INF, 0]]
+sample_a = [[0, 5, NPATH, 10], 
+            [NPATH, 0, 3, NPATH], 
+            [NPATH, NPATH, 0, 1], 
+            [NPATH, NPATH, NPATH, 0]]
+
+output_a = [[0, 5, 8, 9], 
+            [NPATH, 0, 3, 4], 
+            [NPATH, NPATH, 0, 1], 
+            [NPATH, NPATH, NPATH, 0]]
 
 # Test B: Negative Edges Graph (No Negative Cycles)
-sample_b = [[0, 12, INF, 5],
-            [INF, 0, INF, 4],
-            [INF, INF, 0, -2],
-            [INF, INF, 5, 0]]
+sample_b = [[0, 12, NPATH, 5], 
+            [NPATH, 0, NPATH, 4], 
+            [NPATH, NPATH, 0, -2], 
+            [NPATH, NPATH, 5, 0]]
 
-output_b = [[0, 12, 10, 5],
-            [INF, 0, 9, 4],
-            [INF, INF, 0, -2],
-            [INF, INF, 5, 0]]
+output_b = [[0, 12, 10, 5], 
+            [NPATH, 0, 9, 4], 
+            [NPATH, NPATH, 0, -2], 
+            [NPATH, NPATH, 5, 0]]
 
 # Test C: Small Graph (Three Vertices)
-sample_c = [[0, INF, 10],
-            [INF, 0, INF],
-            [INF, 2, 0]]
+sample_c = [[0, NPATH, 10], 
+            [NPATH, 0, NPATH], 
+            [NPATH, 2, 0]]
 
 output_c = [[0, 12, 10],
-            [INF, 0, INF],
-            [INF, 2, 0]]
+            [NPATH, 0, NPATH],
+            [NPATH, 2, 0]]
