50430254438c38ef2b5c549ebeb563b88662c94d
#Sheida <41925038+sheidaj@users.noreply.github.com>
2022-12-03 17:29:18 +0000

    Update Test_sample.py

--- Test/Test_sample.py
+++ Test/Test_sample.py
@@ -1,4 +1,4 @@
-#Sample test run
+# Sample test run
 import unittest
 import sys
 
@@ -11,72 +11,82 @@ from floyd_recur_code import shortpath
 from floyd_recur_code import floyd
 
 
-# Test the main Recursive function
+# Test the main finding the shortest path function
 class testpathSample(unittest.TestCase):
 
     def test_shortpath_function(self):
         NPATH = sys.maxsize
         graphfl = [[0, 7, NPATH, 8],
-                 [NPATH, 0, 5, NPATH],
-                 [NPATH, NPATH, 0, 2],
-                 [NPATH, NPATH, NPATH, 0]]
+                   [NPATH, 0, 5, NPATH],
+                   [NPATH, NPATH, 0, 2],
+                   [NPATH, NPATH, NPATH, 0]]
         result = shortpath(0, 1, 3, graphfl)
-        self.assertEqual(result, 7, "hint if needed")
+        self.assertEqual(result, 7, "should be 7")
 
 
-# Test the Floyd function known as FLW
+# Test the implemantation of recursion in Floyd function
 class ResultTest(unittest.TestCase):
 
-    def test_flw_function(self):
+    def test_fl_function(self):
         NPATH = sys.maxsize
         graphfl = [[0, 7, NPATH, 8],
-                 [NPATH, 0, 5, NPATH],
-                 [NPATH, NPATH, 0, 2],
-                 [NPATH, NPATH, NPATH, 0]]
+                   [NPATH, 0, 5, NPATH],
+                   [NPATH, NPATH, 0, 2],
+                   [NPATH, NPATH, NPATH, 0]]
         MAXL = len(graphfl[0])
-        shortest_graph = [[0, 7, 12, 8],
-                          [NPATH, 0, 5, 7],
-                          [NPATH, NPATH, 0, 2],
-                          [NPATH, NPATH, NPATH, 0]]
+        result_graph = [[0, 7, 12, 8],
+                        [NPATH, 0, 5, 7],
+                        [NPATH, NPATH, 0, 2],
+                        [NPATH, NPATH, NPATH, 0]]
         result = floyd(graphfl)
-        self.assertEqual(result, shortest_graph,"hint if needed")
+        self.assertEqual(result, result_graph,"matching output")
 
     # Test with wrong input
-    # Input a string or a character instead of a integer,
+    # as a string or a character instead of a integer,
     # the test fails
-    def test_inputfailure_function(self):
+    def test_wronginput(self):
         NPATH = sys.maxsize
-        graphfl = [[0, "x", NPATH, 8],
-                 [NPATH, 0, 5, NPATH],
-                 [NPATH, NPATH, 0, 2],
-                 [NPATH, NPATH, NPATH, 0]]
+        graphfl = [[0, "No_Path", "No_Path", 8],
+                   [NPATH, 0, 5, NPATH],
+                   [NPATH, NPATH, 0, 2],
+                   [NPATH, NPATH, NPATH, 0]]
         with self.assertRaises(TypeError):
             result = shorttpath(0, 1, 3, graphfl)
 
-    # testing a use case where there is only one node
-    def test_testonenode_function(self):
-        NPATH = sys.maxsize
-        graphfl = [[0]]
-        result = shortpath(0, 0, 0, graphfl)
-        self.assertEqual(result, 0)
-
-    # Test non-existent node
-    def test_nodepath_function(self):
+ 
+    # Test non-exist node
+    def test_wrongnode(self):
         NPATH = sys.maxsize
         graphfl = [[0, 7, NPATH, 8],
-                 [NPATH, 0, 5, NPATH],
-                 [NPATH, NPATH, 0, 2],
-                 [NPATH, NPATH, NPATH, 0]]
+                   [NPATH, 0, 5, NPATH],
+                   [NPATH, NPATH, 0, 2],
+                   [NPATH, NPATH, NPATH, 0]]
         with self.assertRaises(IndexError):
-            result = shortpath(0, 4, 0, graphfl)
+            result = shortpath(0, 10, 0, graphfl)
 
-    # Test non-existent intermediary node
-    def test_noninternode_function(self):
+    # Test non-exist intermediate node
+    def test_wrongintermediate(self):
         NPATH = sys.maxsize
         graphfl = [[0, 7, NPATH, 8],
-                 [NPATH, 0, 5, NPATH],
-                 [NPATH, NPATH, 0, 2],
-                 [NPATH, NPATH, NPATH, 0]]
+                   [NPATH, 0, 5, NPATH],
+                   [NPATH, NPATH, 0, 2],
+                   [NPATH, NPATH, NPATH, 0]]
         with self.assertRaises(IndexError):
             result = shortpath(0, 1, 4, graphfl)
 
+
+# Unit Tests
+class TestFloyd(unittest.TestCase):
+
+    def test_floyda(self):
+        self.assertEqual(floyd(sample_a), output_a, "Incorrect Output")
+
+    def test_floydb(self):
+        self.assertEqual(floyd(sample_b), output_b, "Incorrect Output")
+
+    def test_floydc(self):
+        self.assertEqual(floyd(sample_c), output_c, "Incorrect Output")
+
+
+if __name__ == '__main__':
+    unittest.main()
